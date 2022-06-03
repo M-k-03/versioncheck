d2b715abd8bb0983933774f1b50cf1609e6eee0f
Muthu <muthukum4r@gmail.com>
2022-06-04 00:02:05 +0530

    initial commit

---
+++ src/main/java/com/example/vercheck/VercheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.vercheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VercheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VercheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vercheck/VercheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vercheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VercheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
